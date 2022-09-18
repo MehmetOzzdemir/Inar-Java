a963e8873d9e50acd9d37851dcbde8a6f15a9550
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-09-18 20:55:10 +0300

    IntelliJ Done

---
+++ src/Chapters/Chapter06/Loop.java
@@ -0,0 +1,16 @@
+package Chapters.Chapter06;
+
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        for(int i=0;i<5;i++){
+        System.out.println("Merhaba Dünya ");
+    }}
+
+
+
+
+
+}
