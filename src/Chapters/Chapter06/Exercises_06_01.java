1d022e361eb2dae3e092c134297a9473fe71366f
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-10-02 00:41:23 +0300

    Done

---
+++ src/Chapters/Chapter06/Exercises_06_01.java
@@ -0,0 +1,10 @@
+package Chapters.Chapter06;
+
+import java.util.Scanner;
+
+public class Exercises_06_01 {
+    public static void main(String[]args){
+        Scanner input =new Scanner(System.in);
+
+    }
+}
