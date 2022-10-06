9aca838bf968ac8dea3ca50c28e120c347a148e3
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-10-06 11:41:38 +0300

    Exercises_06_10 Done

--- src/Chapters/Chapter06/Exercises_06_10.java
+++ src/Chapters/Chapter06/Exercises_06_10.java
@@ -3,7 +3,7 @@ package Chapters.Chapter06;
 public class Exercises_06_10 {
     public static void main(String[] args) {
         int count=0;
-        for(int i=2;i<1000;i++){
+        for(int i=2;i<10000;i++){
             if (IsPrime(i)){
                 count++;
                 System.out.print(i+" ");
