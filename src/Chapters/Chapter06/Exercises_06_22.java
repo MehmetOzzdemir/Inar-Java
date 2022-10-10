f3b5f8cf82bcfdcfaf1bfd7f37a513522d49e9d4
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-10-11 00:03:05 +0300

    Exercise_06_22 done

--- src/Chapters/Chapter06/Exercises_06_22.java
+++ src/Chapters/Chapter06/Exercises_06_22.java
@@ -1,7 +1,26 @@
 package Chapters.Chapter06;
 
+import java.util.Scanner;
+
 public class Exercises_06_22 {
     public static void main(String[] args) {
+        Scanner input =new Scanner (System.in);
+        System.out.print("Enter a Number : ");
+        long number =input.nextLong();
+        System.out.println(number+" is : "+sqrt(number));
+
 
     }
+    public static double sqrt(long n) {
+        long lastGuess = 1;
+        long nextGuess = (lastGuess + n / lastGuess) / 2;
+        while (nextGuess - lastGuess != 0) {
+            lastGuess = nextGuess;
+            nextGuess = (lastGuess + n / lastGuess) / 2;
+
+
+        }
+        return nextGuess;
+    }
+
 }
