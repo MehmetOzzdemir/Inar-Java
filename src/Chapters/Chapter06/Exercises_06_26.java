c338df342a0083e954ae1c75a7abb239d2a3f691
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-10-13 00:28:05 +0300

    Exercises_06_26 Done

--- src/Chapters/Chapter06/Exercises_06_26.java
+++ src/Chapters/Chapter06/Exercises_06_26.java
@@ -4,38 +4,36 @@ public class Exercises_06_26 {
     public static void main(String[] args) {
 
         int count=0;
-        for (int i = 0; count <100 ; i++) {
-            if(isPrime(i)&& isPalindrome(i)){
-                System.out.printf("%10d", i);
+        for (int i = 2; count <100 ; i++) {
+            if(isPrime(i) && isPalindrome(i)){
+                System.out.printf("%8d", i);
                 count++;
-
-
-            if (count % 10 == 0 && i != 0){
+            if (count % 10 == 0){
                 System.out.println();
+                }
             }
-            }
-
         }
-
     }
     public static boolean isPrime(long num){
-        for (int i = 2; i <=num; i++) {
+        for (int i = 2; i <=num/2; i++) {
                 if(num%i==0){
                    return false;
             }
         }
         return true;
     }
-    public static long reverse (long num){
-        long reverse=0;
-        while(num != 0){
-            reverse*=10;
-            reverse +=num%10;
-            num/=10;
+    public static boolean isPalindrome (long num){
+        long reverse = 0;
+        long temp=num;
+        while (num != 0) {
+            reverse *= 10;
+            reverse += num % 10;
+            num /= 10;
         }
-        return reverse;
-    }
-    public static boolean isPalindrome(long num){
-        return  num==reverse(num);
+        if(temp==reverse){ //check palindrome
+            return true;
+        }else {
+            return false;
+        }
     }
 }
