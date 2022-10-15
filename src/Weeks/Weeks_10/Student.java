0d826ea785c1a42fadf1fbeea1cf5639978ab7d3
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-10-15 19:28:54 +0300

    Student done

---
+++ src/Weeks/Weeks_10/Student.java
@@ -0,0 +1,13 @@
+package Weeks.Weeks_10;
+
+public class Student {
+    public static void main(String[] args) {
+        String name;
+        String lastName;
+        int ago;
+        String email;
+    }
+    public void changeEmail(String newEmail){
+        //this.email=newEmail;
+    }
+}
