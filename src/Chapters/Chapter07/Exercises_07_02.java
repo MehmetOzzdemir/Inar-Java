73df25e63454725e7ba986f15ac2057c362b6972
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-10-18 09:37:10 +0300

    Exercises_07_02 Completed

--- src/Chapters/Chapter07/Exercises_07_02.java
+++ src/Chapters/Chapter07/Exercises_07_02.java
@@ -5,6 +5,19 @@ import java.util.Scanner;
 public class Exercises_07_02 {
     public static void main(String[] args) {
         Scanner input =new Scanner(System.in);
-        
+        System.out.print("Enter ten Integers: ");
+
+        int[] ten = new int[10];
+
+
+        for (int i = 0; i < ten.length; i++){
+            int num =(int)(Math.random()*100)+1;
+            ten[i] = num;
+            System.out.print(ten[i]+" ");
+        }
+        System.out.print("\nThe Reverse of Numbers :");
+        for (int i = ten.length - 1; i >= 0; i--) {
+            System.out.print(+ten[i] + " ");
+    }
     }
 }
