8c6b37f60f1cb6f0c5e6bfdba966ee00ce1a9f50
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-10-20 12:46:51 +0300

    Exercises_07_03 Done

--- src/Chapters/Chapter07/Exercises_07_03.java
+++ src/Chapters/Chapter07/Exercises_07_03.java
@@ -8,12 +8,28 @@ public class Exercises_07_03 {
         System.out.print("Enter The Array's Length : ");
         int num =input.nextInt();
         int [] numbers = new int[num];
-        System.out.print("The Integers Between 1 and 100 : ");
+        System.out.print("Enter The Integers Between 1 and 100 : ");
+        int count=0;
+        int number;
+        do {
+            number =input.nextInt();
+            numbers[count]=number;
+            count++;
+        }while (number != 0);
+            countOccurence(numbers);
 
-        for (int i = 0; i <numbers.length ; i++) {
-            int randomNumber =(int)(Math.random()*100)+1;
-            numbers[i]=randomNumber;
-            System.out.printf("%3d",numbers[i]);
+
+    }
+    public static void countOccurence(int[] numbers) {
+        for (int i = 1; i <= 100; i++) {
+            int count = 0;
+            for (int j = 0; j < numbers.length - 1; j++) {
+                if (numbers[j] == i)
+                    count += 1;
+            }
+            if (count != 0)
+                System.out.printf("%d occurs %d %s%n",
+                        i, count, count > 1 ? "times" : "time");
         }
     }
 }
