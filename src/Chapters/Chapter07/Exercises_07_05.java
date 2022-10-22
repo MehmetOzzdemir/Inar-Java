943a0a9ce90acb4f694f0f9fcc41909ae56f7537
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-10-22 16:56:00 +0300

    Exercises_07_05.java Done

--- src/Chapters/Chapter07/Exercises_07_05.java
+++ src/Chapters/Chapter07/Exercises_07_05.java
@@ -6,10 +6,32 @@ public class Exercises_07_05 {
     public static void main(String[] args) {
         Scanner input =new Scanner(System.in);
         int []arr =new int [10];
+
+        int num;
+        int count=0;
+
         System.out.print("Enter Ten Numbers : ");
         for (int i = 0; i <arr.length ; i++) {
-            arr [i]=input.nextInt();
-            System.out.print(arr[i]+" ");
+            num=input.nextInt();
+            if(isDistinct(arr,num)){
+                arr[count]=num;
+                count++;
+            }
         }
+        System.out.println("The Number of Distinct Number is "+count);
+        System.out.print("The Distinct Number are");
+        for (int i = 0; i < arr.length ; i++) {
+            if(arr[i]>0){
+                System.out.print(" "+arr[i]);
+            }
+        }
+
+    }
+    public static boolean isDistinct (int [] arr1, int num){
+        for (int i = 0; i < arr1.length ; i++) {
+            if(num == arr1[i]){
+                return false;
+            }
+        }return true;
     }
 }
