403541bdc87f8323381e9b72c6273b53898ce472
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-10-28 13:53:57 +0300

    Exercises_07_15 Done

--- src/Chapters/Chapter07/Exercises_07_14.java
+++ src/Chapters/Chapter07/Exercises_07_14.java
@@ -5,27 +5,29 @@ import java.util.Scanner;
 public class Exercises_07_14 {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
-        System.out.print("Enter Five Numbers : ");
+        System.out.print("Enter Five Numbers : ");//5 10 15 20 25
         int [] numbers = new int[5];
         for (int i = 0; i <numbers.length ; i++) {
             numbers[i]=input.nextInt();
             System.out.print(numbers[i]+" ");
         }
+
         System.out.println("'s GCD :"+gcd(numbers));
 
     }
     public static int gcd(int... numbers){
         int gcd=1;
-        int count=0;
         int min=min(numbers);
+        int count=0;
         for (int i = 2; i <= min ; i++) {
-            for (int j = 0; j < numbers.length; j++) {
+            for (int j = 0; j <= (numbers.length-1); j++) {
                 if(numbers[j]%i==0){
                     count++;
-                    if(count==(numbers.length-1)){
+                    if(count==(numbers.length)){
                         gcd = i;
-                    }else
-                        count=0;
+                    }
+                }else{
+                    count=0;
                 }
             }
         }
---
+++ src/Chapters/Chapter07/Exercises_07_15.java
@@ -0,0 +1,46 @@
+package Chapters.Chapter07;
+
+public class Exercises_07_15 {
+    public static void main(String[] args) {
+        /*(Eliminate duplicates) Write a method that returns a new array by eliminating the
+        duplicate values in the array using the following method header:
+        public static int[] eliminateDuplicates(int[] list)
+        Write a test program that reads in ten integers, invokes the method, and displays
+        the result. Here is the sample run of the program:
+
+         */
+        int []arr =new int[10];
+        for (int i = 0; i <arr.length ; i++) {
+            arr[i]=(int)((Math.random()+1)*10);
+            System.out.print(arr[i]+  " ");
+        }
+        System.out.println();
+        System.out.print("The distinct numbers are:");
+        for (int e: eliminateDuplicates(arr)) {
+            if (e > 0)
+                System.out.print(" " + e);
+        }
+        System.out.println();
+    }
+    public static int[] eliminateDuplicates(int[] list){
+        int count=0;
+        int[] arr1 = new int [list.length];
+        for (int e :list
+            ) {
+            if(linearSearch(arr1,e)==-1){
+                arr1[count]=e;
+                count++;
+            }
+
+        }
+        return arr1;
+    }
+    public static int linearSearch(int []arr,int key){
+        for (int i = 0; i <arr.length ; i++) {
+            if(key==arr[i]){
+                return key;
+            }
+        }
+        return -1;
+    }
+}
