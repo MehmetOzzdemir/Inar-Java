5632210e46998194a03a22c8d2c12066125d2165
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-11-05 20:24:17 +0300

    Weeks_13 Done

---
+++ src/Weeks/Weeks_13/Practice.java
@@ -0,0 +1,21 @@
+package Weeks.Weeks_13;
+
+import java.math.BigInteger;
+
+public class Practice {
+    public static void main(String[] args) {
+
+        StringBuilder s1=new StringBuilder("Mehmet");
+        s1.append(" Özdemir");//ekleme
+
+
+        String s2="Mehmet";
+        s2 += " Özdemir";
+        System.out.println(s1);
+        System.out.println(s2);
+
+        //BigInteger a=Integer.MAX_VALUE;
+
+
+    }
+}
