a83c3972b5d55b5bd7ab3c51e970813942d7feb4
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-11-10 13:01:57 +0300

    Exercises_09_03/04/05 Done

---
+++ src/Chapters/Chapters09/Exercises_09_04.java
@@ -0,0 +1,16 @@
+package Chapters.Chapters09;
+
+import java.util.Random;
+
+public class Exercises_09_04 {
+    public static void main(String[] args) {
+        Random random =new Random(1000);
+        for (int i = 0; i <50 ; i++) {
+            System.out.printf("%3d",random.nextInt(100));
+
+            if((i+1)%10==0){
+                System.out.println();
+            }
+        }
+    }
+}
---
+++ src/Chapters/Chapters09/Exercises_09_05.java
@@ -0,0 +1,13 @@
+package Chapters.Chapters09;
+
+import java.util.GregorianCalendar;
+
+public class Exercises_09_05 {
+    public static void main(String[] args) {
+        GregorianCalendar date =new GregorianCalendar();
+        System.out.println(date.get(GregorianCalendar.DAY_OF_MONTH)+"/"+date.get(GregorianCalendar.MONTH)+"/"+date.get(GregorianCalendar.YEAR));
+
+        date.setTimeInMillis(1234567898765L);
+        System.out.println(date.get(GregorianCalendar.DAY_OF_MONTH)+"/"+date.get(GregorianCalendar.MONTH)+"/"+date.get(GregorianCalendar.YEAR));
+    }
+}
