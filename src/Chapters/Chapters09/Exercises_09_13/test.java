c31aaf38c4ff7f76640acf85097ef24f907b2589
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-11-12 00:10:25 +0300

    Exercises_09_13 Done

---
+++ src/Chapters/Chapters09/Exercises_09_13/test.java
@@ -0,0 +1,13 @@
+package Chapters.Chapters09.Exercises_09_13;
+
+public class test {
+    public static void main(String[] args) {
+        Location mehmet =new Location();
+        System.out.println(mehmet.toString());
+        mehmet.setRow(35);
+        mehmet.setColumn(25);
+        mehmet.setMaxValue(335);
+        System.out.println(mehmet.toString());
+
+    }
+}
