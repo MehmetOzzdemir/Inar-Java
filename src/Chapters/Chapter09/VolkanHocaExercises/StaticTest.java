32644ec172a79797ce71381075835994fa3bd451
Mehmet <mehmet.ozzdemir@hotmail.com>
2022-11-18 21:40:00 +0300

    Chapter_09 Done

---
+++ src/Chapters/Chapter09/VolkanHocaExercises/StaticTest.java
@@ -0,0 +1,21 @@
+package Chapters.Chapter09.VolkanHocaExercises;
+
+public class StaticTest {
+    public static void main(String[] args) {
+        Data d1 =new Data();
+        Data d2 =new Data();
+        Data d3 =new Data();
+        Data d4 =new Data();
+        Data d5 =new Data();
+        Data d6 =new Data();
+        Data d7 =new Data();
+        Data d8 =new Data();
+        Data d9 =new Data();
+        Data d10 =new Data();
+        Data d11 =new Data();
+
+    }
+}
+class Data{
+    static long [] array = new long[12_500_000];
+}
